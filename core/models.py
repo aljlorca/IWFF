b1edf3ad68f7b23a414ef4c7b4103774f5d38027
Matias <jo.fredesa@alumnos.duoc.cl>
2021-07-21 01:33:11 -0400

    k

--- core/models.py
+++ core/models.py
@@ -31,7 +31,7 @@ class producto(models.Model):
     stock = models.IntegerField()
     stock_critico = models.IntegerField()
     proveedor = models.ForeignKey(proveedor, on_delete=models.PROTECT)
-    imagen = models.ImageField(upload_to='productos', null=True, blank=True)
+    imagen = models.ImageField(upload_to='productos', null=True)
     oferta = models.BooleanField(default='0', null=False)
 
     def __str__(self):
