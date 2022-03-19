44b0200b8830ce0d3a3aee895419f0d7b5c992d5
Alan Esinoza Lorca <alan.espinoza.1133@gmail.com>
2022-03-19 10:16:35 -0300

    Agregado de sql y usuario para facilitar la carga de db

--- IWFF/settings.py
+++ IWFF/settings.py
@@ -94,14 +94,9 @@ WSGI_APPLICATION = 'IWFF.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xe',
+        'NAME': 'localhost:1521/xe',
         'USER': 'C##FERME',
-        'PASSWORD': '1133',
-        'TEST': {
-            'USER': 'default_test',
-            'TBLSPACE': 'default_test_tbls',
-            'TBLSPACE_TMP': 'default_test_tbls_tmp',
-        },
+        'PASSWORD': '1133', 
     },  
     
 }
